2740d2167218fe9b8b4caae66633064bd04bed6a
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-10-24 19:27:27 +0900

    빌드전 리덕스 버전 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { applyMiddleware, createStore } from 'redux';
+import {createStore } from 'redux';
 import rootReducer from './reducers'
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
-    console.log("store", store);
-    console.log("action", action);
-    next(action);
-}
-
-
-const middleware = applyMiddleware(thunk, loggerMiddleware);
-const store = createStore(rootReducer, middleware);
-
-
+const store = createStore(rootReducer);
 
 const render = () => root.render(
     <React.StrictMode>
